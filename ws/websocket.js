0e803a5d040cb7c283d8c1e4fbe26b50e1e549f6
cncp20 <cp8888@88.com>
2025-05-02 20:09:50 +0800

    UPDATE

--- ws/websocket.js
+++ ws/websocket.js
@@ -42,80 +42,82 @@ class ws {
                 let nowType = null;
                 let tmpWeight = 0;
 				this.client.on("message", (topic, payload) => {
-
-
-                    let data = JSON.parse(payload.toString());
-					let arr = data["weight_data"] || [];
-					let currentPlant = arr.filter((item) => item.note === "1");
-					if (currentPlant.length===1) {
-						// 有人在上面
-                        let {id, status} = currentPlant[0];
-						if (id === "dq_weight" && status === "0") {
-							// 正在称重
-                            if (nowStatus === 0) {
-                                console.log(1, payload.toString());
-                                nowStatus = 1;
-                                ws.send(
-                                    JSON.stringify({
-                                        code: 1,
-                                    })
-                                );
-                            }
-						} else {
-							// 数值稳定称重完成
-                            console.log(2, payload.toString());
-                            nowStatus = 2;
-                            let weight = 0;
-                            let type = "";
-                            if (id === "dq_weight") {
-                                type = "earth";
-                            } else if (id === "Venus1") {
-                                type = "venus";
-                            } else {
-                                type = id.toLowerCase();
-                            }
-                            if (nowType !== type) {
-                                // 星球切换了才发送消息给前端
-                                nowType = type;
-                                if (id === "dq_weight") {
-                                    weight = currentPlant[0].weight / 1000; //kg
-                                    tmpWeight = weight;
-                                    setTimeout(() => {
-                                        tmpWeight = 0;
-                                        nowType = null;
-                                    }, 30000);
-                                } else {
-                                    weight = tmpWeight;
+                    try {
+                        let data = JSON.parse(payload.toString());
+                        let arr = data["weight_data"] || [];
+                        let currentPlant = arr.filter((item) => item.note === "1");
+                        if (currentPlant.length===1) {
+                            // 有人在上面
+                            let {id, status} = currentPlant[0];
+                            if (id === "dq_weight" && status === "0") {
+                                // 正在称重
+                                if (nowStatus === 0) {
+                                    console.log(1, payload.toString());
+                                    nowStatus = 1;
+                                    ws.send(
+                                        JSON.stringify({
+                                            code: 1,
+                                        })
+                                    );
                                 }
-                                
-                                let obj = {};
-                                Object.entries(weightMap).map(([key, value]) => {
-                                    obj[key] = (value * weight).toFixed(0);
-                                });
+                            } else {
+                                // 数值稳定称重完成
+                                console.log(2, payload.toString());
+                                nowStatus = 2;
+                                let weight = 0;
+                                let type = "";
+                                if (id === "dq_weight") {
+                                    type = "earth";
+                                } else if (id === "Venus1") {
+                                    type = "venus";
+                                } else {
+                                    type = id.toLowerCase();
+                                }
+                                if (nowType !== type) {
+                                    // 星球切换了才发送消息给前端
+                                    nowType = type;
+                                    if (id === "dq_weight") {
+                                        weight = currentPlant[0].weight / 1000; //kg
+                                        tmpWeight = weight;
+                                        setTimeout(() => {
+                                            tmpWeight = 0;
+                                            nowType = null;
+                                        }, 30000);
+                                    } else {
+                                        weight = tmpWeight;
+                                    }
+                                    
+                                    let obj = {};
+                                    Object.entries(weightMap).map(([key, value]) => {
+                                        obj[key] = (value * weight).toFixed(0);
+                                    });
+                                    ws.send(
+                                        JSON.stringify({
+                                            code: 2,
+                                            type: Object.keys(weightMap).findIndex(
+                                                (item) => item === type
+                                            ),
+                                            weight: obj,
+                                        })
+                                    );
+                                }
+
+                            }
+                        } else {
+                            // 没人在上面或者正在移动
+                            if (nowStatus !== 0 && !tmpWeight) {
+                                console.log(0, payload.toString());
+                                nowStatus = 0;
                                 ws.send(
                                     JSON.stringify({
-                                        code: 2,
-                                        type: Object.keys(weightMap).findIndex(
-                                            (item) => item === type
-                                        ),
-                                        weight: obj,
+                                        code: 0,
                                     })
                                 );
                             }
-
-						}
-					} else {
-						// 没人在上面或者正在移动
-                        if (nowStatus !== 0 && !tmpWeight) {
-                            console.log(0, payload.toString());
-                            nowStatus = 0;
-                            ws.send(
-                                JSON.stringify({
-                                    code: 0,
-                                })
-                            );
                         }
-					}
+                    } catch (error) {
+                        console.log("mqtt error", payload.toString());
+                    }
 				});
 			} catch (error) {
 				console.log("websocket connection error", error);
